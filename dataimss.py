fc09c85488604c8ac96df495586f6de20ceeae1f
lmanuel_ExM <lmanuel.links@gmail.com>
2022-10-17 11:03:57 -0500

    update

--- dataimss.py
+++ dataimss.py
@@ -7,7 +7,7 @@ __author__ = 'lguerrero'
 class DataImss():
     _uma = 96.22 # <- apartir de 1 febrero 2022. 89.62 - 2021
     dias = 30.4
-    _prima_de_riesgo = 0.54355
+    _prima_de_riesgo = 0.50000 #0.54355
 
     def __init__(self) -> None:
         self._sdi = 0

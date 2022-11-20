d7a176939ce0357e542696d898372b3ced397c27
Luis Manuel <lmanuel.links@gmail.com>
2022-11-19 23:29:24 -0600

    correcciónes aguinaldo

--- isr_aguinaldo.py
+++ isr_aguinaldo.py
@@ -23,6 +23,10 @@ class Aguinaldo(DataImss):
         self._aguinaldo = aguinaldo
 
     def aguinaldo_grabado(self) -> float:
+        '''
+        Aguinaldo Exento. Hasta 30 UMAs (96.44* x 30)
+        Aguinaldo Gravado (Aguinaldo Total- Aguinaldo Exento)
+        '''
         excento = self._uma * 30
         if (self._aguinaldo - excento) > 0:
             return self._aguinaldo - excento
@@ -50,28 +54,55 @@ class Reglamento(ISR, Aguinaldo):
         # self._aguinaldo = 0
 
     def fracc_i(self) -> float:
+        '''
+        Art. 174 Fracción I. La remuneración (Aguinaldo GRAVADO) de que se trate 
+        se dividirá entre 365 y el resultado se multiplicará por 30.4;
+        '''
         return round(self.aguinaldo_grabado() / 365, 2)
 
     def fracc_ii(self) -> float:
+        '''
+        Art. 174 Fracción II. A la cantidad que se obtenga conforme a la fracción anterior, se le 
+        adicionará el ingreso ordinario por la prestación de un servicio personal subordinado que 
+        perciba el trabajador en forma regular en el mes de que se trate y al resultado se le 
+        aplicará el procedimiento establecido en el artículo 96 de la Ley;
+        '''
         remuneracion = self.get_sueldo_base() + self.fracc_i()
         ingreso_aguinaldo = ISR()
         ingreso_aguinaldo.set_sueldo_base(remuneracion)
         return round(ingreso_aguinaldo.calculo_impuesto(), 2)
 
     def fracc_iii(self) -> float:
+        '''
+        Art. 174 Fracción III. El Impuesto que se obtenga conforme a la fracción anterior se 
+        disminuirá con el Impuesto que correspondería al ingreso ordinario por la prestación de 
+        un servicio personal subordinado a que se refiere dicha fracción, calculando este último 
+        sin considerar las demás remuneraciones mencionadas en este artículo.
+        '''
         return round(self.fracc_ii() - self.calculo_impuesto(), 4)
 
     def fracc_v(self) -> float:
+        '''
+        Art. 174 Fracción V. La tasa a que se refiere la fracción anterior, se calculará dividiendo 
+        el Impuesto que se determine en términos de la fracción III de este artículo entre la 
+        cantidad que resulte conforme a la fracción I de dicho artículo. El cociente se multiplicará 
+        por cien y el producto se expresará en por ciento
+        '''
         if self.fracc_i() > 0:
             return round(self.fracc_iii() / self.fracc_i(), 4)
         else:
             return 0
 
     def fracc_iv(self) -> float:
+        '''
+        Art. 174 Fracción IV. El Impuesto a retener será el que resulte de aplicar a las 
+        remuneraciones a que se refiere este artículo, sin deducción alguna, la tasa a que se 
+        refiere la fracción siguiente, y
+        '''
         return round(self.aguinaldo_grabado() * self.fracc_v(), 2)
     
 
-class Imuesto_Aguinaldo(Ley, Reglamento):
+class Impuesto_Aguinaldo(Ley, Reglamento):
     def __init__(self):
         super().__init__()
 
@@ -79,19 +110,25 @@ class Imuesto_Aguinaldo(Ley, Reglamento):
         if self.fracc_iv() > self.impuesto_aguinaldo_ley():
             return self.impuesto_aguinaldo_ley()
         else:
-            self.fracc_iv()
+            return self.fracc_iv()
+
+    def texto(self):
+        if self.fracc_iv() > self.impuesto_aguinaldo_ley():
+            return "Impuesto de Ley"
+        else:
+            return "Impuesto de Reglamento"
 
 
-# if __name__ == '__main__':
-#     calculo = Imuesto_Aguinaldo()
-#     calculo.set_aguinaldo(3526.04)
-#     print(calculo.fracc_i())
-#     calculo.set_sueldo_base(19500)
-#     print(calculo.get_sueldo_base())
-#     print(calculo.fracc_ii())
-#     print(calculo.fracc_iii())
-#     print(calculo.fracc_v())
-#     print(calculo.fracc_iv())
-#     print(calculo.aguinaldo_grabado())
-#     print(calculo.impuesto_aguinaldo_ley())
-#     print(calculo.impuesto_aguinaldo())
+if __name__ == '__main__':
+    calculo = Impuesto_Aguinaldo()
+    calculo.set_aguinaldo(3526.04)
+    # print(calculo.fracc_i())
+    # calculo.set_sueldo_base(19500)
+    # print(calculo.get_sueldo_base())
+    # print(calculo.fracc_ii())
+    # print(calculo.fracc_iii())
+    # print(calculo.fracc_v())
+    # print(calculo.fracc_iv())
+    print(calculo.aguinaldo_grabado())
+    # print(calculo.impuesto_aguinaldo_ley())
+    # print(calculo.impuesto_aguinaldo())

d102d83864c5c31f49d551f39f7b607dc7eef84e
Manu_ExM <lmanuel.links@gmail.com>
2024-10-22 16:13:00 -0600

    mejoras a piramidación gracias a ChatGPT

--- piramidacion.py
+++ piramidacion.py
@@ -11,7 +11,8 @@ class Piramidacion(CalculoIMSSObrero,ISR,SBC):
         self.fecha_fin_ejercicio = pd.to_datetime('2024/12/31')
 
     def calculo_vales(self, gross_salary:float) -> int:
-        return int(gross_salary * 0.05) if int(gross_salary * 0.05) <= 1500 else 1500
+        vales = int(gross_salary * 0.05)
+        return min(vales, 1500)
     
     def get_net_salary(self, gross_salary:float, with_vouchers:bool) -> float:
         vales = self.calculo_vales(gross_salary)
@@ -19,20 +20,14 @@ class Piramidacion(CalculoIMSSObrero,ISR,SBC):
         sbc = SBC.funcion_sbc(gross_salary, vales=vales, admission_date=self.fecha_inicio_tranajador, base_date=self.fecha_fin_ejercicio)
         imss = CalculoIMSSObrero.function_imss_obrero(sbc)
 
-        if with_vouchers == 1:
-            neto = gross_salary - isr - imss + vales
-        else:
-            neto = gross_salary - isr - imss  
+        neto = gross_salary - isr - imss + vales if with_vouchers else gross_salary - isr - imss
 
         return round(neto,2) 
 
-    def approach_gross_salary(self, goal:float, vales=0) -> float:
-        
-        inicio = goal * 1.42
-        
+    def approach_gross_salary(self, goal:float, vales=0) -> float:      
+        inicio = goal * 1.42        
         while  goal < self.get_net_salary(inicio, vales):
             inicio -= 1
-
         return inicio
     
     def df_iteraciones(self, rango:int, incremento:float, approach_income:float, with_vouchers=0):
@@ -59,13 +54,11 @@ class Piramidacion(CalculoIMSSObrero,ISR,SBC):
         imss_obs = [CalculoIMSSObrero.function_imss_obrero(sbc) for sbc in sbcs] 
 
         # sixth
-        net_salary = []
-        if with_vouchers == 1:
-            for sueldo, vale, isr, imss in zip(sueldos, vouchers, isrs, imss_obs):
-                net_salary.append(sueldo - isr - imss + vale)
+        if with_vouchers:
+            net_salary = [sueldo - isr - imss + vale for sueldo, vale, isr, imss in zip(sueldos, vouchers, isrs, imss_obs)]
         else:
-            for sueldo, vale, isr, imss in zip(sueldos, isrs, imss_obs):
-                net_salary.append(sueldo - isr - imss)
+            net_salary = [sueldo - isr - imss  for sueldo, isr, imss in zip(sueldos, vouchers, isrs, imss_obs)]
+
         
         data_frame = {'Sueldo':sueldos,'Vales':vouchers,'SBC':sbcs,'ISR':isrs,'IMSS':imss_obs,'Neto':net_salary}
 
@@ -74,7 +67,8 @@ class Piramidacion(CalculoIMSSObrero,ISR,SBC):
     def df_ta(self, buscar_valor:float, with_vouchers=0):
         sueldo_inicial = self.approach_gross_salary(buscar_valor, with_vouchers) - 5
         df = self.df_iteraciones(1000, 0.01, sueldo_inicial, with_vouchers)
-        df = df[(df['Neto']>=(buscar_valor-0.01*2))&(df['Neto']<=(buscar_valor+0.01*3))]
+        margen = 0.01  # Ajuste de margen para mayor precisión
+        df = df[(df['Neto'] >= (buscar_valor - margen)) & (df['Neto'] <= (buscar_valor + margen))]
         self.df_approach = df.copy()
 
     def putout_ta(self):

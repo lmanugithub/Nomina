436b31b732058376489f4896f17a66bf73d6b23d
Manu_ExM <lmanuel.links@gmail.com>
2024-10-22 13:40:56 -0600

    update: piramidación falta revisar y mejorar

--- piramidacion.py
+++ piramidacion.py
@@ -11,7 +11,7 @@ class Piramidacion(CalculoIMSSObrero,ISR,SBC):
         self.fecha_fin_ejercicio = pd.to_datetime('2024/12/31')
 
     def calculo_vales(self, gross_salary:float) -> int:
-        return int(gross_salary * 0.05 if gross_salary * 0.05 <= 1500 else 1500)
+        return int(gross_salary * 0.05) if int(gross_salary * 0.05) <= 1500 else 1500
     
     def get_net_salary(self, gross_salary:float, with_vouchers:bool) -> float:
         vales = self.calculo_vales(gross_salary)
@@ -26,7 +26,7 @@ class Piramidacion(CalculoIMSSObrero,ISR,SBC):
 
         return round(neto,2) 
 
-    def get_approch_to_gross_salary(self, goal:float, vales=0) -> float:
+    def approach_gross_salary(self, goal:float, vales=0) -> float:
         
         inicio = goal * 1.42
         
@@ -35,10 +35,59 @@ class Piramidacion(CalculoIMSSObrero,ISR,SBC):
 
         return inicio
     
+    def df_iteraciones(self, rango:int, incremento:float, approach_income:float, with_vouchers=0):
+        
+        # first 
+        sueldos = []
+        for i in range(1,rango):
+            approach_income += incremento
+            sueldos.append(approach_income)
+        
+        # second
+        vouchers = [int(sueldo * 0.05) if int(sueldo * 0.05) <= 1500 else 1500 for sueldo in sueldos]
+
+        # third
+        isrs = list(map(lambda sueldo: ISR.function_isr(sueldo), sueldos))
+
+        # fourth
+        sbcs = []
+        for sueldo, vale in zip(sueldos,vouchers):
+            value_sbc = SBC.funcion_sbc(sueldo=sueldo, vales=vale, admission_date=self.fecha_inicio_tranajador, base_date=self.fecha_fin_ejercicio)
+            sbcs.append(value_sbc)
+        
+        # fifth
+        imss_obs = [CalculoIMSSObrero.function_imss_obrero(sbc) for sbc in sbcs] 
+
+        # sixth
+        net_salary = []
+        if with_vouchers == 1:
+            for sueldo, vale, isr, imss in zip(sueldos, vouchers, isrs, imss_obs):
+                net_salary.append(sueldo - isr - imss + vale)
+        else:
+            for sueldo, vale, isr, imss in zip(sueldos, isrs, imss_obs):
+                net_salary.append(sueldo - isr - imss)
+        
+        data_frame = {'Sueldo':sueldos,'Vales':vouchers,'SBC':sbcs,'ISR':isrs,'IMSS':imss_obs,'Neto':net_salary}
+
+        return pd.DataFrame(data_frame)
+    
+    def df_ta(self, buscar_valor:float, with_vouchers=0):
+        sueldo_inicial = self.approach_gross_salary(buscar_valor, with_vouchers) - 5
+        df = self.df_iteraciones(1000, 0.01, sueldo_inicial, with_vouchers)
+        df = df[(df['Neto']>=(buscar_valor-0.01*2))&(df['Neto']<=(buscar_valor+0.01*3))]
+        self.df_approach = df.copy()
+
+    def putout_ta(self):
+        return self.df_approach
+    
+
+
 if __name__=='__main__':
     valor_a_buscar = 13000
     approach = Piramidacion()
-    print(approach.get_approch_to_gross_salary(valor_a_buscar,0))
+    print(approach.approach_gross_salary(valor_a_buscar,1))
+    approach.df_ta(valor_a_buscar, 1)
+    print(approach.putout_ta())
     
 
 
